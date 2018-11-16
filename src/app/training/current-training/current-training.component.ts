103d771ab07e5267b50eefd0b52ba3ccc66bb8d5
devlap <nascal3@gmail.com>
2018-11-17 01:06:00 +0300

    added dialog to the stop button

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {StopTrainingComponent} from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -7,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: number;
 
-  constructor() { }
+  constructor(private dialoge: MatDialog) { }
 
   ngOnInit() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
     }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    this.dialoge.open(StopTrainingComponent);
   }
 
 }

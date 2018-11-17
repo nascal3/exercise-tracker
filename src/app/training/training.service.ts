3ad1376bf5898dc4fb783b4892df1b11fd3c6c47
devlap <nascal3@gmail.com>
2018-11-17 17:16:18 +0300

    added an emit of the selected exercise from the training service using 'Subject'

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,7 +1,8 @@
-
+import { Subject } from 'rxjs';
 import {ExerciseModel} from './exercise.model';
 
 export class TrainingService {
+  exerciseChanged = new Subject<ExerciseModel>();
   private availableExercises: ExerciseModel[] = [
     {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
     {id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
@@ -17,5 +18,6 @@ export class TrainingService {
 
   startExercise(selectedId: string) {
     this.runningExcersice = this.availableExercises.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({...this.runningExcersice});
   }
 }

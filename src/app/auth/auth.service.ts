277dc95d57d0b2b5cd3c847943282a12056559fd
devlap <nascal3@gmail.com>
2018-11-17 15:05:32 +0300

    added added redirect when user logs in or out.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,22 @@
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import {UserModel} from './user.model';
 import {AuthDataModel} from './auth-data.model';
+import {Router} from '@angular/router';
 
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: UserModel;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthDataModel) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   login(authData: AuthDataModel) {
@@ -19,12 +24,13 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
@@ -33,5 +39,10 @@ export class AuthService {
 
   isAuth() {
     return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 }

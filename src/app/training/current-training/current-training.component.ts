e8979e38940100b16c924286ac70dd7b76ce08c8
devlap <nascal3@gmail.com>
2018-11-20 01:07:04 +0300

    removed emit method to cancel excersice and replaced it with 'completeExcercise' from training service.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {StopTrainingComponent} from './stop-training.component';
 import {TrainingService} from '../training.service';
@@ -9,7 +9,7 @@ import {TrainingService} from '../training.service';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
+
   progress = 0;
   timer: number;
 
@@ -24,6 +24,7 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = setInterval(() => {
       this.progress = this.progress + 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExcercise();
         clearInterval(this.timer);
       }
     }, step);
@@ -37,7 +38,7 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe( result => {
       if (result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress);
       } else {
         this.startOrResumeTimer();
       }

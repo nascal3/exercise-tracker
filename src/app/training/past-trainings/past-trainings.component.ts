aaa3f48e7501581f5fa3cd4a5afeb63dc9a54494
devlap <nascal3@gmail.com>
2018-11-20 02:55:43 +0300

    added pagination  into material table data

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatSort, MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {ExerciseModel} from '../exercise.model';
 import {TrainingService} from '../training.service';
 
@@ -11,17 +11,22 @@ import {TrainingService} from '../training.service';
 export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<ExerciseModel>();
+  dataLength: number;
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedAndCancelledExercises();
+    this.dataLength = this.dataSource.data.length;
+    console.log('>>>', this.dataSource.data.length);
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(filterValue: string) {

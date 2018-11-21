7f29e11c02339d71a24b6e69b90cd1c6cc1a4fdf
devlap <nascal3@gmail.com>
2018-11-21 16:47:21 +0300

    emit values of finished exercises from firebase

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,17 +1,19 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {ExerciseModel} from '../exercise.model';
 import {TrainingService} from '../training.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.scss']
 })
-export class PastTrainingsComponent implements OnInit, AfterViewInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<ExerciseModel>();
   dataLength: number;
+  private subscription: Subscription;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -19,7 +21,10 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
-    this.dataSource.data = this.trainingService.getCompletedAndCancelledExercises();
+    this.subscription = this.trainingService.finishedExercisesChanged.subscribe((exercises: ExerciseModel[]) => {
+      this.dataSource.data = exercises;
+    });
+    this.trainingService.fetchCompletedAndCancelledExercises();
     this.dataLength = this.dataSource.data.length;
   }
 
@@ -30,5 +35,9 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
   doFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }

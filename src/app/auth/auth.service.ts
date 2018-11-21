259639c42664ad943efd8b5a5e7df482909d9159
devlap <nascal3@gmail.com>
2018-11-21 17:55:04 +0300

    call subscription method to close all subscriptions on logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,13 +4,14 @@ import {UserModel} from './user.model';
 import {AuthDataModel} from './auth-data.model';
 import {Router} from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import {TrainingService} from '../training/training.service';
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth) {}
+  constructor(private router: Router, private afAuth: AngularFireAuth, private trainingService: TrainingService) {}
 
   registerUser(authData: AuthDataModel) {
     this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then(result => {
@@ -32,6 +33,8 @@ export class AuthService {
   }
 
   logout() {
+    this.trainingService.cancelSubscriptions();
+    this.afAuth.auth.signOut();
     this.authChange.next(false);
     this.router.navigate(['/login']);
     this.isAuthenticated = true;

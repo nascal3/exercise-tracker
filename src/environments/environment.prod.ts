52431812f6731ba7e9c808f173c6220bbd9c4c5c
devlap <nascal3@gmail.com>
2018-11-26 00:44:29 +0300

    added ngrx package to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAtXvknz8sRVAuq7lhjMnnQbQyHmP35xQs',
+    authDomain: 'ng-fitness-tracker-5fce5.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-5fce5.firebaseio.com',
+    projectId: 'ng-fitness-tracker-5fce5',
+    storageBucket: 'ng-fitness-tracker-5fce5.appspot.com',
+    messagingSenderId: '79452831318'
+  }
 };

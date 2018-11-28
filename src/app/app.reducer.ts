875ee92a1b12dae2c5b2592d5db181969927105a
devlap <nascal3@gmail.com>
2018-11-28 16:08:29 +0300

    added auth reducer to app reducer

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,13 +1,19 @@
-import * as fromUi from './shared/ui.reducer';
 import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+import * as fromUi from './shared/ui.reducer';
+import * as fromAuth from './auth/auth.reducer';
 
 export interface State {
   ui: fromUi.State;
+  auth: fromAuth.State;
 }
 
-export const reducers: ActionReducerMap<State> = {
-  ui: fromUi.uiReducer
+export const reducers: ActionReducerMap<any> = {
+  ui: fromUi.uiReducer,
+  auth: fromAuth.getIsAuth
 };
 
 export const getUiState = createFeatureSelector<fromUi.State>('ui');
 export const getIsLoaidng = createSelector(getUiState, fromUi.getIsLoading);
+
+export const getAuthSate = createFeatureSelector<fromAuth.State>('auth');
+export const getisAuth = createSelector(getAuthSate, fromAuth.getIsAuth);

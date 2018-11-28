80e2307c5a22920dff2ce06ebcb8b6b0937236cf
devlap <nascal3@gmail.com>
2018-11-28 18:45:39 +0300

    set up training reducer and actions

---
+++ src/app/training/training.actions.ts
@@ -0,0 +1,31 @@
+import { Action } from '@ngrx/store';
+import {ExerciseModel} from './exercise.model';
+
+export const SET_AVAILABLE_TRAININGS = '[Training] Set Available Trainings';
+export const SET_FINISHED_TRAININGS = '[Training] Set Finished Trainings';
+export const START_TRAINING = '[Training] Start Training';
+export const STOP_TRAINING = '[Training] Stop Training';
+
+export class SetAvailableTriainings implements Action {
+  readonly type = SET_AVAILABLE_TRAININGS;
+
+  constructor(public payload: ExerciseModel[]) {}
+}
+
+export class SetFinishedTrainings implements Action {
+  readonly type = SET_FINISHED_TRAININGS;
+
+  constructor(public payload: ExerciseModel[]) {}
+}
+
+export class StartTraining implements Action {
+  readonly type = START_TRAINING;
+
+  constructor(public payload: ExerciseModel) {}
+}
+
+export class StopTraining implements Action {
+  readonly type = STOP_TRAINING;
+}
+
+export type TrainingActions = SetAvailableTriainings | SetFinishedTrainings | StartTraining | StopTraining;

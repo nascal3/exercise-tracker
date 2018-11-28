783100613aa0042864afaec42380de83b321ff4b
devlap <nascal3@gmail.com>
2018-11-28 17:54:07 +0300

    added call to store to get state from the store

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,26 +1,20 @@
 import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
-import {AuthService} from './auth.service';
+import {select, Store} from '@ngrx/store';
+import * as fromRoot from '../app.reducer';
+import {take} from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private store: Store<fromRoot.State>, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isAuth()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-    }
+    return this.store.pipe(select(fromRoot.getisAuth), take(1));
   }
 
   canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isAuth()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-    }
+    return this.store.pipe(select(fromRoot.getisAuth), take(1));
   }
 
 }
